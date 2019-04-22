2bb9bc702d8a7dee2d35ef15d5534e73b0b024d6
xjl;gvho;idfsjhlkfdsjhlk <thomas.colomba30@gmail.com>
2019-04-22 21:41:02 +0200

    first commit

---
+++ ejemplo2/src/components/HelloWorld/HelloWorld.js
@@ -0,0 +1,8 @@
+export default {
+  name: 'HelloWorld',
+  data : function () {
+    return {
+  		myMsg:'Hello you !'
+  	}
+  }
+}
---
+++ ejemplo2/src/components/HelloWorld/HelloWorld.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="hello">
+    <h1>{{ myMsg }}</h1>
+  </div>
+</template>
+<script src="./HelloWorld.js"></script>
+<style src="./HelloWorld.css"></style>
